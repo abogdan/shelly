2751b0970919ec15ee87a1a8695482e30326bcda
Bogdan Andritoiu <bogdan@andritoiu.ro>
2014-12-11 23:57:56 +0200

    Added licence, headers and README

--- src/Executor.php
+++ src/Executor.php
@@ -1,4 +1,12 @@
 <?php
+/*
+* This file is part of Shelly.
+*
+* (c) Bogdan Andritoiu <bogdan@andritoiu.ro>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 namespace ABogdan\Shelly;
 
